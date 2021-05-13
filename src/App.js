134a17f5560d747a2a93b7e91be6f383e3a6e2dc
adithanbs <MY_NAME@example.com>
2021-05-13 13:36:35 +0530

    plain_navbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-
-import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./Header";
-import './App.css';
-
+import "./App.css";
 
 function App() {
   return (
     <Router>
       <div className="app">
-              <Switch>
-
-                <Route path="/">
-                  <Header/>
-                </Route>
-                
-              </Switch>
+        <Switch>
+        <Route path="/">
+            <Header />
+            </Route>
+        </Switch>
       </div>
     </Router>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,26 +1,24 @@
 import React from 'react'
-import Header from "./Header.css"
-import { Link } from "react-router-dom"
-import SearchIcon from '@material-ui/icons/Search';
+import SearchIcon from "@material-ui/icons/Search";
 
-export const Header = () => {
+import { Link } from 'react-router-dom'
+import "./Header.css"
+const Header = () => {
     return (
-        <div classname="header">
-<Link to="/">
-<img
-    classname="header__logo"
-    src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-    alt=""
-/>            
-</Link>  
-<div classname="header__search">
-<input type="text" classname="header__SearchInput"/>
-<SearchIcon classname="SearchIcon" />
-</div>
+        <div className="header">
+            <Link to="/">
+                <img
+                    className="header__logo"
+                    src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                    alt="Amazon"     
+                />
+            </Link>
+            <div className="header__search">
+                <input type="text" className="header__searchInput"/>
+                 <SearchIcon className="header__searchIcon"/>
+            </div>
         </div>
-        // search box
-        // 3 links
-        // Basket icon with number
-        )}
-        
+    )
+}
+
 export default Header
